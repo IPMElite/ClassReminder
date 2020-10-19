fa61464bf41cf6a232742b5ca5ab28cc81e18e99
IPMElite <73038323+IPMElite@users.noreply.github.com>
2020-10-19 16:21:27 -0500

    Update ClassReminder.py

--- ClassReminder.py
+++ ClassReminder.py
@@ -28,10 +28,7 @@ bot = discord.Client()
 #		await asyncio.sleep(5)
 #		await bot.delete_message(message)
 #		await asyncio.sleep(4)
-async def on_ready():
-	channel = bot.get_channel(767857071944892426)
-	role = discord.utils.get(
-	message = await channel.send("Please react to this message if you wish to subscribe to reminders for each period.")
+
 async def time_check():
 	print("Running time check")
 	await bot.wait_until_ready()
@@ -81,6 +78,10 @@ async def time_check():
 		elif d == "Sat" or d == "Sun":
 			print("Weekend")
 			await asyncio.sleep(1800)
+async def on_ready():
+	channel = bot.get_channel(767857071944892426)
+	role = discord.utils.get(
+	message = await channel.send("Please react to this message if you wish to subscribe to reminders for each period.")
 
 bot.loop.create_task(time_check())
 bot.run(token)
