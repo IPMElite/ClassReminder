708b53d44c2387feca03cced2f4c86487d76336d
IPMElite <73038323+IPMElite@users.noreply.github.com>
2020-10-20 17:15:46 -0500

    Update ClassReminder.py

--- ClassReminder.py
+++ ClassReminder.py
@@ -12,8 +12,6 @@ from discord.ext import commands
 num = 60
 token = os.environ.get('BOT_TOKEN')
 bot = discord.Client()
-#role = msg.channel.server.roles.mention('name', 'Subscribed')
-role = discord.utils.get(ctx.guild.roles, id=767857989460819980)
 @bot.event
 @bot.event
 #msg = "This is an automatic message per launch and will be deleted after a few seconds."
@@ -32,8 +30,7 @@ role = discord.utils.get(ctx.guild.roles, id=767857989460819980)
 #		await asyncio.sleep(4)
 async def on_ready():
 	channel = bot.get_channel(767857071944892426)
-	message = await channel.send("Use the command '!subscribe' to subscribe to reminders" + role.mention())
-	await ctx.send(role.mention())
+	message = await channel.send("Use the command '!subscribe' to subscribe to reminders")
 #	await bot.delete_message(message)
 async def on_message(message):
 	print("Checking for !subscribe")
@@ -61,52 +58,52 @@ async def time_check():
 		#channel = bot.get_channel(discord.utils.get(server.channels, name = "reminders"))
 		if (d == "Mon" or d == "Tue" or d == "Wed" or d == "Thu" or d == "Fri") and (today != 2020-11-3 or today != 2020-11-26 or today != 2020-11-27 or today != 2020-12-17 or today != 2020-12-18 or today != 2020-12-21 or today != 2020-12-22 or today != 2020-12-23 or today != 2020-12-24):
 			if d == "Wed" and current_time >= "07:53:30" and current_time <= "07:54:30":
-				await channel.send( "FBLA is starting in a minute. <@767857989460819980>")
+				await channel.send( "FBLA is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(fbla)
 #				await asyncio.sleep(60)
 			if current_time >= "08:33:30" and current_time <= "08:34:30":
-				await channel.send("Period 1 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 1 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(first)
 #				await asyncio.sleep(60)
 			if current_time >= "09:29:30" and current_time <= "09:30:30":
-				await channel.send("Period 2 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 2 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(second)
 #				await asyncio.sleep(60)
 			if current_time >= "10:20:30" and current_time <= "10:21:30":
-				await channel.send("Period 3 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 3 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(third)
 #				await asyncio.sleep(60)
 			if current_time >= "11:13:30" and current_time <= "11:14:30":
-				await channel.send("Period 4 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 4 is starting in a minute.>")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(fourth)
 #				await asyncio.sleep(60)
 			if current_time >= "12:05:30" and current_time <= "12:06:30":
-				await channel.send("Period 5 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 5 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(fifth)
 #				await asyncio.sleep(60)
 			if current_time >= "12:57:30" and current_time <= "12:58:30":
-				await channel.send("Period 6 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 6 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(sixth)
 #				await asyncio.sleep(60)
 			if current_time >= "13:49:30" and current_time <= "13:50:30":
-				await channel.send("Period 7 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 7 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(seventh)
 #				await asyncio.sleep(60)
 			if current_time >= "14:41:30" and current_time <= "14:42:30":
-				await channel.send("Period 8 is starting in a minute. <@767857989460819980>")
+				await channel.send("Period 8 is starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(eighth)
 #				await asyncio.sleep(60)
 			if d == "Mon" and current_time >= "15:43:30" and current_time <= "15:44:30":
-				await channel.send("Computer Science Club in starting in a minute. <@767857989460819980>")
+				await channel.send("Computer Science Club in starting in a minute.")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(csc)
 #				await asyncio.sleep(60)
@@ -116,7 +113,7 @@ async def time_check():
 #				await bot.delete_message(cscover)
 #				await asyncio.sleep(60)
 			if d == "Thu" and current_time >= "15:28:30" and current_time <= "15:29:30":
-				await channel.send("History Bee/Bowl is starting in a minute. <@767857989460819980>")
+				await channel.send("History Bee/Bowl is starting in a minute. ")
 #				await asyncio.sleep(60)
 #				await bot.delete_message(hbb)
 #				await asyncio.sleep(60)
