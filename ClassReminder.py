c8d435cac25dd9beec03733a0b74abc8894fff2b
IPMElite <73038323+IPMElite@users.noreply.github.com>
2020-10-24 13:53:46 -0500

    Update ClassReminder.py

--- ClassReminder.py
+++ ClassReminder.py
@@ -38,12 +38,11 @@ async def time_check():
 		role = get(guild.roles, name='Subscribed')
 		channel = bot.get_channel(767855423775572018)
 		channel2 = bot.get_channel(discord.utils.get(guild.channels, name = "reminders")
-		if current_time >= "13:50:30" and current_time <= "13:51:30":
-			await channel.send("Test")
-			await channel.send(role.mention)
-			await asyncio.sleep(60)
-			async for msg in channel.history(limit = 50):
-				await msg.delete()
+		await channel2.send("Test")
+		await channel2.send(role.mention)
+		await asyncio2.sleep(60)
+		async for msg in channel2.history(limit = 50):
+			await msg.delete()
 		if (d == "Mon" or d == "Tue" or d == "Wed" or d == "Thu" or d == "Fri") and (today != 2020-11-3 or today != 2020-11-26 or today != 2020-11-27 or today != 2020-12-17 or today != 2020-12-18 or today != 2020-12-21 or today != 2020-12-22 or today != 2020-12-23 or today != 2020-12-24):
 			if d == "Wed" and current_time >= "07:53:30" and current_time <= "07:54:30":
 				await channel.send( "FBLA is starting in a minute. ")
